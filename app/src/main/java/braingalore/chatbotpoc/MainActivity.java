b493d7c09443e4525557606e805b36d9b655b133
s92 <s92@avaya.com>
2017-11-24 16:17:14 +0530

    Speech to text implementation

--- app/src/main/java/braingalore/chatbotpoc/MainActivity.java
+++ app/src/main/java/braingalore/chatbotpoc/MainActivity.java
@@ -1,21 +1,32 @@
 package braingalore.chatbotpoc;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Context;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.text.Html;
-import android.view.KeyEvent;
 import android.view.View;
-import android.view.inputmethod.EditorInfo;
-import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageButton;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.ibm.watson.developer_cloud.android.library.audio.MicrophoneInputStream;
+import com.ibm.watson.developer_cloud.android.library.audio.utils.ContentType;
 import com.ibm.watson.developer_cloud.conversation.v1.ConversationService;
 import com.ibm.watson.developer_cloud.conversation.v1.model.MessageRequest;
 import com.ibm.watson.developer_cloud.conversation.v1.model.MessageResponse;
 import com.ibm.watson.developer_cloud.http.ServiceCallback;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.SpeechToText;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.model.RecognizeOptions;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.model.SpeechResults;
+import com.ibm.watson.developer_cloud.speech_to_text.v1.websocket.RecognizeCallback;
 
 public class MainActivity extends AppCompatActivity {
+
+    private EditText userInput;
+    private ImageButton recordMessage;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,9 +40,10 @@ public class MainActivity extends AppCompatActivity {
                         getString(R.string.password)
                 );
 
-        final TextView conversation = (TextView)findViewById(R.id.conversation);
-        final EditText userInput = (EditText)findViewById(R.id.user_input);
-        final Button sendMessage = (Button)findViewById(R.id.send);
+        final TextView conversation = (TextView) findViewById(R.id.conversation);
+        userInput = (EditText) findViewById(R.id.user_input);
+        final ImageButton sendMessage = (ImageButton) findViewById(R.id.send);
+        recordMessage = (ImageButton) findViewById(R.id.record);
         sendMessage.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -60,23 +72,162 @@ public class MainActivity extends AppCompatActivity {
                             }
 
                             @Override
-                            public void onFailure(Exception e) {}
+                            public void onFailure(Exception e) {
+                            }
                         });
                 // Optionally, clear edittext
                 userInput.setText("");
             }
         });
-        /*userInput.setOnEditorActionListener(new TextView
-                .OnEditorActionListener() {
+        recordMessage.setOnClickListener(new View.OnClickListener() {
             @Override
-            public boolean onEditorAction(TextView tv,
-                                          int action, KeyEvent keyEvent) {
-                if(action == EditorInfo.IME_ACTION_DONE) {
-
-                }
-                return false;
+            public void onClick(View v) {
+                recordMessage();
             }
-        });*/
+        });
+    }
+
+    //Private Methods - Speech to Text
+    private RecognizeOptions getRecognizeOptions() {
+        return new RecognizeOptions.Builder()
+                .continuous(true)
+                .contentType(ContentType.OPUS.toString())
+                //.model("en-UK_NarrowbandModel")
+                .interimResults(true)
+                .inactivityTimeout(2000)
+                .build();
+    }
+
+    //Watson Speech to Text Methods.
+    private class MicrophoneRecognizeDelegate implements RecognizeCallback {
+        @Override
+        public void onTranscription(SpeechResults speechResults) {
+            System.out.println(speechResults);
+            if (speechResults.getResults() != null && !speechResults.getResults().isEmpty()) {
+                String text = speechResults.getResults().get(0).getAlternatives().get(0).getTranscript();
+                showMicText(text);
+            }
+        }
+
+        @Override
+        public void onConnected() {
+        }
+
+        @Override
+        public void onError(Exception e) {
+            showError(e);
+            enableMicButton();
+        }
+
+        @Override
+        public void onDisconnected() {
+            enableMicButton();
+        }
+
+        @Override
+        public void onInactivityTimeout(RuntimeException runtimeException) {
+
+        }
+
+        @Override
+        public void onListening() {
+
+        }
+
+        /*@Override
+        public void onInactivityTimeout(RuntimeException runtimeException) {
+
+        }
+
+        @Override
+        public void onListening() {
+
+        }*/
+    }
+
+    private void showMicText(final String text) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                userInput.setText(text);
+            }
+        });
+    }
+
+    private void enableMicButton() {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                recordMessage.setEnabled(true);
+            }
+        });
+    }
+
+    private void showError(final Exception e) {
+        runOnUiThread(new Runnable() {
+            @Override
+            public void run() {
+                Toast.makeText(MainActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                e.printStackTrace();
+            }
+        });
+    }
+
+    private SpeechToText speechService;
+    private MicrophoneInputStream capture;
+    private boolean listening = false;
+
+    //Record a message via Watson Speech to Text
+    private void recordMessage() {
+        //mic.setEnabled(false);
+        speechService = new SpeechToText();
+        speechService.setUsernameAndPassword("f9591925-b4d7-4d08-9898-06b4271eb5ea", "FF8Zuu7tUWCF");
+        if (listening != true) {
+            capture = new MicrophoneInputStream(true);
+            new Thread(new Runnable() {
+                @Override
+                public void run() {
+                    try {
+                        speechService.recognizeUsingWebSocket(capture, getRecognizeOptions(), new MicrophoneRecognizeDelegate());
+                    } catch (Exception e) {
+                        showError(e);
+                    }
+                }
+            }).start();
+            listening = true;
+            Toast.makeText(this, "Listening....Click to Stop", Toast.LENGTH_SHORT).show();
+        } else {
+            try {
+                capture.close();
+                listening = false;
+                Toast.makeText(this, "Stopped Listening....Click to Start", Toast.LENGTH_SHORT).show();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    /**
+     * Check Internet Connection
+     *
+     * @return
+     */
+    private boolean checkInternetConnection() {
+        // get Connectivity Manager object to check connection
+        ConnectivityManager cm =
+                (ConnectivityManager) getSystemService(Context.CONNECTIVITY_SERVICE);
+
+        NetworkInfo activeNetwork = cm.getActiveNetworkInfo();
+        boolean isConnected = activeNetwork != null &&
+                activeNetwork.isConnectedOrConnecting();
+
+        // Check for network connections
+        if (isConnected) {
+            return true;
+        } else {
+            Toast.makeText(this, " No Internet Connection available ", Toast.LENGTH_SHORT).show();
+            return false;
+        }
 
     }
 }
